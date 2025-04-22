a33808c6a3a8ccd90dbaa34446fb8c56cb03ae49
hansol <fpdlqm21@naver.com>
2025-04-22 15:26:20 +0900

    first commit

---
+++ backend/src/main/java/com/hansol/hansol/HansolApplication.java
@@ -0,0 +1,13 @@
+package com.hansol.hansol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HansolApplication.class, args);
+	}
+
+}
