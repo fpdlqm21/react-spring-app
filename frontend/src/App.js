356180887dfcfdc0f5916a573ca5d2ec83bdd58d
KimSeongWon <kimsungwon57@naver.com>
2025-05-20 18:01:27 +0900

    메인화면test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 // App.js
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import axios from "axios";
 import Home from "./pages/Home";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="" element={<Main/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-          <Route path={"/Home"} element={<Home />} />
+        <Route path={"/Home"} element={<Home />} />
       </Routes>
     </Router>
   );
